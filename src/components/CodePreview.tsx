46be4957410dd3f4fcf2f962c4a3cc241b740f83
pudtipong.pa <pudtipong.pa@bdms.co.th>
2025-05-20 14:59:47 +0700

    feat: add copy code button in code preview

--- src/components/CodePreview.tsx
+++ src/components/CodePreview.tsx
@@ -1,11 +1,40 @@
+import { useState } from 'react';
+import { TbCopy, TbCopyCheck } from "react-icons/tb";
+
 interface Props {
     code: string;
 }
 
 export default function CodePreview({ code }: Props) {
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = async () => {
+        try {
+            await navigator.clipboard.writeText(code);
+            setCopied(true);
+            setTimeout(() => setCopied(false), 2000);
+        } catch (err) {
+            console.error('Failed to copy!', err);
+        }
+    };
+
     return (
-        <pre className="bg-gray-800 text-green-200 p-4 rounded overflow-auto whitespace-pre-wrap">
-            {code}
-        </pre>
+        <div className="relative">
+            {copied ? (
+                <TbCopyCheck
+                    className="absolute top-2 right-2 text-green-400"
+                    size={25}
+                />
+            ) : (
+                <TbCopy
+                    className="absolute top-2 right-2 text-slate-200 hover:text-white"
+                    onClick={handleCopy}
+                    size={25}
+                />
+            )}
+            <pre className="bg-gray-800 text-green-200 p-4 min-h-10 rounded overflow-auto whitespace-pre-wrap">
+                {code}
+            </pre>
+        </div>
     );
 }
