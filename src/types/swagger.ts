6098ffd33dbcff084ff1b80c77350bdea45354ec
pudtipong.pa <pudtipong.pa@bdms.co.th>
2025-05-20 13:52:17 +0700

    refactor: swagger types

--- src/types/swagger.ts
+++ src/types/swagger.ts
@@ -1,9 +1,20 @@
-export interface SwaggerInput {
+export interface SwaggerSchemaProperty {
+    type: string;
+    description: string;
+    example?: string | number | boolean;
+  }
+  
+  export interface SwaggerResponse {
+    description: string;
+  }
+  
+  export interface SwaggerInput {
     path: string;
     method: 'get' | 'post' | 'put' | 'delete';
     tags: string[];
     summary: string;
     description: string;
-    requestBody?: Record<string, unknown>;
-    responses?: Record<string, unknown>;
-}
+    requestBody?: Record<string, SwaggerSchemaProperty>;
+    responses?: Record<string, SwaggerResponse>;
+  }
+  
