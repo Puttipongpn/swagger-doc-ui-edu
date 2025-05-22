cead63197c1187f46a6b8897b06b31f764ac22b4
pudtipong.pa <pudtipong.pa@bdms.co.th>
2025-05-22 10:18:22 +0700

    feat: add download functionality for code preview in CodePreview component

--- src/components/CodePreview.tsx
+++ src/components/CodePreview.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { TbCopy, TbCopyCheck } from "react-icons/tb";
+import { MdOutlineFileDownload } from "react-icons/md";
+import { MdOutlineFileDownloadDone } from "react-icons/md";
 
 interface Props {
     code: string;
@@ -7,6 +9,7 @@ interface Props {
 
 export default function CodePreview({ code }: Props) {
     const [copied, setCopied] = useState(false);
+    const [downloaded, setDownloaded] = useState(false);
 
     const handleCopy = async () => {
         try {
@@ -17,6 +20,18 @@ export default function CodePreview({ code }: Props) {
             console.error('Failed to copy!', err);
         }
     };
+
+    const handelDownload = () => {
+        const blob = new Blob([code], { type: 'text/plain' });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'swagger-code.txt';
+        link.click();
+        URL.revokeObjectURL(url);
+        setDownloaded(true);
+        setTimeout(() => setDownloaded(false), 2000);
+    }
 
     return (
         <div className="relative">
@@ -32,6 +47,21 @@ export default function CodePreview({ code }: Props) {
                     size={25}
                 />
             )}
+            {downloaded ? (
+                <MdOutlineFileDownloadDone
+                    className="absolute top-2 right-10 text-green-400"
+                    size={27}
+                />
+            ) :
+                <button
+                    className="absolute top-2 right-10 text-slate-200 hover:text-white"
+                    onClick={handelDownload}
+                >
+                    <MdOutlineFileDownload
+                        size={27}
+                    />
+                </button>}
+
             <pre className="bg-gray-800 text-green-200 p-4 min-h-10 rounded overflow-auto whitespace-pre-wrap">
                 {code}
             </pre>
